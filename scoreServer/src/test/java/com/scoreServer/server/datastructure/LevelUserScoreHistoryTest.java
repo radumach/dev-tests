ca554c5aa09569122393fc3fb78c08f6c0055d46
Shivan Dragon <shivan.dragon@rocketmail.com>
2016-02-25 20:48:10 +0100

    added LevelUserScoreHistory UTs

--- scoreServer/src/test/java/com/scoreServer/server/datastructure/LevelUserScoreHistoryTest.java
+++ scoreServer/src/test/java/com/scoreServer/server/datastructure/LevelUserScoreHistoryTest.java
@@ -1,9 +1,6 @@
 package com.scoreServer.server.datastructure;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.Set;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -57,27 +54,64 @@ public class LevelUserScoreHistoryTest {
 	}
 	
 	@Test
-	public void testHighscoreListFull() {
+	public void testHighscoreListFullNoDuplicates() {
 		scoreHistory.update(new UserScore(1, 55));
 		scoreHistory.update(new UserScore(2, 15));
 		scoreHistory.update(new UserScore(3, 65));
 		scoreHistory.update(new UserScore(4, 25));
 		scoreHistory.update(new UserScore(5, 25));
+		
 		scoreHistory.update(new UserScore(6, 25));
 		scoreHistory.update(new UserScore(7, 25));
 		scoreHistory.update(new UserScore(8, 25));
-		scoreHistory.update(new UserScore(9, 25));
+		scoreHistory.update(new UserScore(9, 35));
 		scoreHistory.update(new UserScore(10, 25));
+		
 		scoreHistory.update(new UserScore(11, 25));
-		scoreHistory.update(new UserScore(12, 25));
+		scoreHistory.update(new UserScore(12, 5));
 		scoreHistory.update(new UserScore(13, 25));
-		scoreHistory.update(new UserScore(14, 25));
-		scoreHistory.update(new UserScore(15, 25));
+		scoreHistory.update(new UserScore(14, 65));
+		scoreHistory.update(new UserScore(15, 55));
 		
 		assertEquals(15, scoreHistory.getHighscores().size());
 		assertEquals(65, scoreHistory.getHighscores().get(0).getScore());
 		assertEquals(3, scoreHistory.getHighscores().get(0).getUserId(), 0);
-		assertEquals(55, scoreHistory.getHighscores().get(1).getScore());
-		assertEquals(1, scoreHistory.getHighscores().get(1).getUserId(), 0);
+		assertEquals(65, scoreHistory.getHighscores().get(1).getScore());
+		assertEquals(14, scoreHistory.getHighscores().get(1).getUserId(), 0);
+		assertEquals(55, scoreHistory.getHighscores().get(2).getScore());
+		assertEquals(1, scoreHistory.getHighscores().get(2).getUserId(), 0);
+	}
+	
+	@Test
+	public void testHighscoreListFullWithDuplicates() {
+		scoreHistory.update(new UserScore(1, 55));
+		scoreHistory.update(new UserScore(2, 15));
+		scoreHistory.update(new UserScore(3, 65));
+		scoreHistory.update(new UserScore(4, 25));
+		scoreHistory.update(new UserScore(5, 25));
+		
+		scoreHistory.update(new UserScore(6, 25));
+		scoreHistory.update(new UserScore(7, 25));
+		scoreHistory.update(new UserScore(8, 25));
+		scoreHistory.update(new UserScore(9, 35));
+		scoreHistory.update(new UserScore(10, 25));
+		
+		scoreHistory.update(new UserScore(5, 15));
+		scoreHistory.update(new UserScore(4, 5));
+		scoreHistory.update(new UserScore(4, 25));
+		scoreHistory.update(new UserScore(2, 105));
+		scoreHistory.update(new UserScore(1, 55));
+		
+		scoreHistory.update(new UserScore(11, 25));
+		scoreHistory.update(new UserScore(12, 5));
+		scoreHistory.update(new UserScore(13, 25));
+		scoreHistory.update(new UserScore(14, 65));
+		scoreHistory.update(new UserScore(15, 55));
+		
+		assertEquals(15, scoreHistory.getHighscores().size());
+		assertEquals(105, scoreHistory.getHighscores().get(0).getScore());
+		assertEquals(2, scoreHistory.getHighscores().get(0).getUserId(), 0);
+		assertEquals(65, scoreHistory.getHighscores().get(1).getScore());
+		assertEquals(3, scoreHistory.getHighscores().get(1).getUserId(), 0);
 	}
 }
